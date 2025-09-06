1f7bf90be11b560081baa2ee0e01797766f0c121
Ahmed1010 <Ahmed01020238429@gmail.com>
2025-09-06 17:44:29 +0300

    Add EF and Make dbContext

---
+++ Minimal API/NewFolder/Context/PizzaContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Minimal_API.NewFolder.Classes.Model;
+
+namespace Minimal_API.NewFolder.Context
+{
+    public class PizzaContext:DbContext
+    {
+        
+        public PizzaContext(DbContextOptions<PizzaContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Pizza> Pizzas { get; set; }
+
+    }
+}
+    
