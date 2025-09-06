73ac9bd075e75bda10c97d2ef179dc5d0d1727b6
Ahmed1010 <Ahmed01020238429@gmail.com>
2025-09-06 16:05:41 +0300

    Swagger initialization

--- Minimal API/Program.cs
+++ Minimal API/Program.cs
@@ -15,7 +15,11 @@ namespace Minimal_API
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             //   builder.Services.AddSwaggerGen();
-          
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ATOS Todo API", Description = "Keep track of your tasks", Version = "v1" });
+            });
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -23,7 +27,11 @@ namespace Minimal_API
             {
                 
                 app.UseSwagger();
-               
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "ATOS API V1");
+                });
+
 
             }
 
