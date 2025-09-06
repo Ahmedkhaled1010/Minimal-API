44755d2de047eb36c1a3eb8b7af9c7313342952c
Ahmed1010 <Ahmed01020238429@gmail.com>
2025-09-06 17:06:00 +0300

    Create Services Interface

---
+++ Minimal API/NewFolder/Interfaces/IPizzaServices.cs
@@ -0,0 +1,13 @@
+﻿using Minimal_API.NewFolder.Classes.Model;
+
+namespace Minimal_API.NewFolder.Interfaces
+{
+    public interface IPizzaServices
+    {
+        List<Pizza> GetPizzas();
+        Pizza? GetPizza(int id);
+        Pizza CreatePizza(Pizza pizza);
+        Pizza UpdatePizza(Pizza update);
+        void RemovePizza(int id);
+    }
+}
